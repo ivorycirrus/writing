9231e429d9ded269c512b7f6659d9d81efdf7862
ivorycirrus <ivorycirrus@gmail.com>
2016-08-01 12:41:07 +0900

    js bigint > factorial 예제 수정

--- laboratory/javascript/big_integer/factorial.js
+++ laboratory/javascript/big_integer/factorial.js
@@ -1,11 +1,9 @@
 function facrotials(n){
-	var result = "\n==[ factorials ]========\n";
 	var fac = 1;
 	var facBigInt = new BigInt(1);
 	for(var inx = 1 ; inx <= n ; inx++ ){
 		fac *= inx; facBigInt = facBigInt.multiply(new BigInt(inx));
-		result += (inx+"!  =>  actual : "+fac+"  / bigint : "+facBigInt.toString()+"\n");
 	}
 
-	return result;
+	return facBigInt.toString();
 }
